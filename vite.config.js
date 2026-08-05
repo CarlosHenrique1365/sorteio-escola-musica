982fc923db796e4be981b24deb61a9a87e7b41af
Carlos Henrique <ferreiradossantosc13@gmail.com>
2026-08-05 17:28:26 -0300

    Configurações para hospedagem

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ export default defineConfig({
   plugins: [react()],
   base: "/sorteio-escola-musica/",
 });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+// });
